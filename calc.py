63cb0e86c344cd230ea42435d2b05d2cd58ec001
bandinfinite <kailash.162k7@gmail.com>
2023-12-03 06:55:03 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,12 @@
+from tkinter import *
+import math
 
+window = Tk()
+window.geometry("300x400")
+window.title("Calculator")
+
+entry = Entry(window, width = 30)
+entry.pack()
+
+btnframe = Frame(window)
+window.mainloop()
